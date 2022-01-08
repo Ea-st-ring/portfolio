2069ebefab4800c44e53b0481dc3023f2003c2c7
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2022-01-08 20:20:48 +0900

    Js : HomeContactBtn

--- main.js
+++ main.js
@@ -20,7 +20,17 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     } // navbar에 undefined 가 뜨는 부분을 없애주는 작업
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
+    scrollIntoView(link);
 });
+
+
+// Handle click on Contact me button on home
+const contactBtn = document.querySelector('.profile__contactBtn');
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#Contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
