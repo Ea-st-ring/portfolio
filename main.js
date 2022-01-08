81d3c081add0731bbf6bb0477c5ae70d7af50ecc
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2022-01-08 19:41:20 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    } // navbar에 undefined 가 뜨는 부분을 없애주는 작업
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+});
